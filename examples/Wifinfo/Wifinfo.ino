78edc1b9151528efcb8b452085628e42a798bf09
pbac <pcassab@subeo.net>
2018-12-16 17:37:26 +0100

    debug print

--- examples/Wifinfo/Wifinfo.ino
+++ examples/Wifinfo/Wifinfo.ino
@@ -809,7 +809,7 @@ void loop()
   if ( Serial.available() ) {
     // Read Serial and process to tinfo
     c = Serial.read();
-    //Serial1.print(c);
+    Serial1.print(c);
     tinfo.process(c);
   }
 
